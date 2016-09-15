3a64578be1272df454b567919e4dba2f06cee19f
fantay <laurentcoll@hotmail.com>
2016-09-15 17:01:05 +0200

    seconde version avec le debut des collections

---
+++ src/test1/Collections.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Laurent-LIM
+ */
+public class Collections {
+
+    public static void main(String[] args) {
+
+        // creation des listes
+        ArrayList<Integer> listeMulti10 = new ArrayList<>();
+        ArrayList<Integer> listeMulti2 = new ArrayList<>();
+        ArrayList<Integer> listeMulti3 = new ArrayList<>();
+
+        //remplissage des listes
+        //liste multiple de 10
+        for (int i = 10; i < 100; i = i + 10) {
+            listeMulti10.add(i);
+        }
+        //liste multiple de 2
+        for (int i = 2; i < 100; i = i + 2) {
+            listeMulti2.add(i);
+        }
+        //liste multiple de 3
+        for (int i = 3; i < 99; i = i + 3) {
+            listeMulti3.add(i);
+        }
+
+        listeMulti2.removeAll(listeMulti10);
+        listeMulti2.removeAll(listeMulti3);
+        
+        System.out.println(listeMulti2);
+    }
+}
