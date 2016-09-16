0be1c8211d331777706ca4775579116c2bf5b802
fantay <laurentcoll@hotmail.com>
2016-09-16 14:39:14 +0200

    version 5 generation des classe private avec getter et setter

---
+++ src/test1/classes/Film.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1.classes;
+
+/**
+ *
+ * @author Laurent-LIM
+ */
+public class Film {
+    
+    // variable de la classe film
+    private String titre;
+    private Integer dureeEnMinute;
+    private String Synopsis;
+    private Integer annee;
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public Integer getDureeEnMinute() {
+        return dureeEnMinute;
+    }
+
+    public void setDureeEnMinute(Integer dureeEnMinute) {
+        this.dureeEnMinute = dureeEnMinute;
+    }
+
+    public String getSynopsis() {
+        return Synopsis;
+    }
+
+    public void setSynopsis(String Synopsis) {
+        this.Synopsis = Synopsis;
+    }
+
+    public Integer getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(Integer annee) {
+        this.annee = annee;
+    }
+    
+
+    
+    
+}
